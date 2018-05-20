1c95c518e232e5956af54c6a6aea30cedef3aaf4
Jeremy Chen <jeremy886@gmail.com>
2018-05-20 21:05:21 +1000

    Complte vote list and vote detail

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.renderers import (
+    BrowsableAPIRenderer,
+    JSONRenderer,
+    TemplateHTMLRenderer,
+)
 
-# Create your views here.
+
+from django.shortcuts import redirect
+from .models import Vote
+from .serializers import VoteSerializer
+
+
+class VoteList(generics.ListCreateAPIView):
+    renderer_classes = (
+        BrowsableAPIRenderer,
+        JSONRenderer,
+        TemplateHTMLRenderer,
+    )
+    template_name = "vote_list.html"
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super(VoteList, self).create(request, *args, **kwargs)
+
+        if request.accepted_renderer.format == 'html' and response.status_code == 201:
+            return redirect('/votes/')
+        return response
+
+
+class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    renderer_classes = (
+        BrowsableAPIRenderer,
+        JSONRenderer,
+        TemplateHTMLRenderer,
+    )
+    template_name = "vote.html"
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
