9c4a541cca47de1aa3e4ff246856e2061311be9e
Jeremy Chen <jeremy886@gmail.com>
2018-05-20 21:57:08 +1000

    Use mixins and add authentication

---
+++ README.rst
@@ -0,0 +1,21 @@
+
+I include sqlite database for you to use.
+
+* superuser: jeremy
+* password: pahgie0Eey6puquaediangaidaije2li
+
+You should have access to the video tutorial on YouTube: https://youtu.be/w0xgJ5C9Be8
+
+It would be easier to follow the code if you download the `slides`_.
+
+.. _slides: https://speakerd.s3.amazonaws.com/presentations/38241b415b184be998476e2d678d0c78/APIDrivenDjangoPycon2018.pdf
+
+
+Test the API:
+
+* http://127.0.0.1:8000/votes/
+* http://127.0.0.1:8000/admin/
+* http://127.0.0.1:8000/votes/?format=json
+* http://127.0.0.1:8000/votes/?format=api
+* http://127.0.0.1:8000/votes/?format=html
+
--- votes/views.py
+++ votes/views.py
@@ -4,37 +4,66 @@ from rest_framework.renderers import (
     JSONRenderer,
     TemplateHTMLRenderer,
 )
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from django.shortcuts import redirect
 from .models import Vote
 from .serializers import VoteSerializer
 
 
-class VoteList(generics.ListCreateAPIView):
-    renderer_classes = (
-        BrowsableAPIRenderer,
-        JSONRenderer,
-        TemplateHTMLRenderer,
-    )
-    template_name = "vote_list.html"
-    queryset = Vote.objects.all()
+# class VoteList(generics.ListCreateAPIView):
+#     renderer_classes = (
+#         BrowsableAPIRenderer,
+#         JSONRenderer,
+#         TemplateHTMLRenderer,
+#     )
+#     template_name = "vote_list.html"
+#     queryset = Vote.objects.all()
+#     serializer_class = VoteSerializer
+#
+#     def create(self, request, *args, **kwargs):
+#         response = super(VoteList, self).create(request, *args, **kwargs)
+#
+#         if request.accepted_renderer.format == 'html' and response.status_code == 201:
+#             return redirect('/votes/')
+#         return response
+
+
+class VoteAPIMixin:
+    render_classes = [JSONRenderer, TemplateHTMLRenderer]
+    queryset = Vote.objects.all().order_by("vote_taken")
     serializer_class = VoteSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_renderers(self):  # not working? anyone can still access API
+        render_classes = self.render_classes
+        if self.request.user.is_staff:
+            render_classes += [BrowsableAPIRenderer]
+        return [renderer() for renderer in render_classes]
+
+
+class VoteList(VoteAPIMixin, generics.ListCreateAPIView):
+
+    template_name = "vote_list.html"
 
     def create(self, request, *args, **kwargs):
         response = super(VoteList, self).create(request, *args, **kwargs)
 
-        if request.accepted_renderer.format == 'html' and response.status_code == 201:
-            return redirect('/votes/')
+        if request.accepted_renderer.format == "html" and response.status_code == 201:
+            return redirect("/votes/")
         return response
 
 
-class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
-    renderer_classes = (
-        BrowsableAPIRenderer,
-        JSONRenderer,
-        TemplateHTMLRenderer,
-    )
+# class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
+#     renderer_classes = (
+#         BrowsableAPIRenderer,
+#         JSONRenderer,
+#         TemplateHTMLRenderer,
+#     )
+#     template_name = "vote.html"
+#     queryset = Vote.objects.all()
+#     serializer_class = VoteSerializer
+
+
+class VoteDetail(VoteAPIMixin, generics.RetrieveUpdateDestroyAPIView):
     template_name = "vote.html"
-    queryset = Vote.objects.all()
-    serializer_class = VoteSerializer
